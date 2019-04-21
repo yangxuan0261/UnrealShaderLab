81767530ad5d545ced34937b3d34fd935e29ba61
wilker <364105996@qq.com>
2019-04-22 00:03:22 +0800

    - init

---
+++ Source/UnrealShaderLab/UnrealShaderLab.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealShaderLab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealShaderLab, "UnrealShaderLab" );
